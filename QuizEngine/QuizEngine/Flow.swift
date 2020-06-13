fc6c0262c862d7f17b2495a63872fc6ff0093a1f
Hikmat <asephikmatf@gmail.com>
2020-06-13 17:37:45 +0700

    create initial engine with test #pt1

---
+++ QuizEngine/QuizEngine/Flow.swift
@@ -0,0 +1,29 @@
+//
+//  Flow.swift
+//  QuizEngine
+//
+//  Created by Fatah on 13/06/20.
+//  Copyright © 2020 Fatah. All rights reserved.
+//
+
+import Foundation
+
+protocol Router {
+    func routeTo(question: String)
+}
+
+class Flow {
+    let router: Router
+    let questions: [String]
+    
+    init(questions: [String], router: Router) {
+        self.questions = questions
+        self.router = router
+    }
+    
+    func start() {
+        if !questions.isEmpty {
+            router.routeTo(question: "")
+        }
+    }
+}
---
+++ QuizEngine/QuizEngine/QuizEngine.h
@@ -0,0 +1,19 @@
+//
+//  QuizEngine.h
+//  QuizEngine
+//
+//  Created by Fatah on 13/06/20.
+//  Copyright © 2020 Fatah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuizEngine.
+FOUNDATION_EXPORT double QuizEngineVersionNumber;
+
+//! Project version string for QuizEngine.
+FOUNDATION_EXPORT const unsigned char QuizEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizEngine/PublicHeader.h>
+
+
