ba3d072fe2e86f2628457556a5fd3878f7cd4fc6
Hikmat <asephikmatf@gmail.com>
2020-06-21 09:03:44 +0700

    make it able to route to next question

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 protocol Router {
-    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
+    typealias AnswerCallback = (String) -> Void
+    
+    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
 }
 
 class Flow {
@@ -23,12 +25,16 @@ class Flow {
     
     func start() {
         if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion) { [weak self] _ in
-                guard let strongSelf = self else { return }
-                let firstQuestionIndex = strongSelf.questions.firstIndex(of: firstQuestion)!
-                let nextQuestion = strongSelf.questions[firstQuestionIndex+1]
-                strongSelf.router.routeTo(question: nextQuestion) { _ in }
-            }
+            router.routeTo(question: firstQuestion, answerCallback: routeNext(firstQuestion))
+        }
+    }
+    
+    func routeNext(_ question: String) -> Router.AnswerCallback {
+        return { [weak self] _ in
+            guard let strongSelf = self else { return }
+            let currentQuestionIndex = strongSelf.questions.firstIndex(of: question)!
+            let nextQuestion = strongSelf.questions[currentQuestionIndex+1]
+            strongSelf.router.routeTo(question: nextQuestion, answerCallback: strongSelf.routeNext(nextQuestion))
         }
     }
 }
--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -57,6 +57,16 @@ class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
     }
     
+    func test_startAndAnswerFirstAndSecondQuestion_withThreeQuestions_routesToSecondAndThirdQuestion() {
+        let sut = makeSUT(questions: ["Q1", "Q2", "Q3"])
+        sut.start()
+        
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+
+        XCTAssertEqual(router.routedQuestions, ["Q1", "Q2", "Q3"])
+    }
+    
     // MARK: Helpers
     
     func makeSUT(questions: [String]) -> Flow {
@@ -67,7 +77,7 @@ class FlowTest: XCTestCase {
         var routedQuestions: [String] = []
         var answerCallback: ((String) -> Void) = { _ in }
         
-        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+        func routeTo(question: String, answerCallback: @escaping Router.AnswerCallback) {
             routedQuestions.append(question)
             self.answerCallback = answerCallback
         }
